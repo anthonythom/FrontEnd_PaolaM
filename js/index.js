3cd132a91f44392a1dabe9abca00edf80fd035cf
anthonythom <anthonythomascontato@gmail.com>
2024-04-22 19:35:00 -0300

    att

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    tel: document.getElementById("tel").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+const serviceID = "service_ifjhn3m";
+const templateID = "template_mkptz6p";
+
+emailjs.send(serviceID, templateID, params)
+  .then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("tel").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("sua mensagem foi enviada com sucesso!");
+  })
+  .catch((err) => console.log(err));
+}
